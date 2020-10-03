52584498777100d2f164d871c9f436c20a56e1b7
Veronika <v.kyxta@gmail.com>
2020-10-03 21:32:53 +0300

    feat: typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
+  if(array == undefined || array.length === 0) {
   return 0;
+  } else { let minValue = array[0];
+    for (let i = 0; i<array.length; i++) {
+      if (array[i]<minValue)
+    {
+    minValue = array[i];
+    }
+  }
+  return minValue;
 }
+    
+};
 
 exports.max = function max (array) {
+  if (array == undefined || array.length === 0) {
+    return 0;
+  }  else { let maxValue = array[0];
+    for (let i = 0; i<array.length; i++) {
+      if (array[i] >= maxValue) {
+        maxValue = array[i];
+        }
+      }
+    return maxValue;
+  }
+  };
+exports.avg = function avg (array) {
+  if (array == undefined || array.length === 0) {
   return 0;
+} let avgValue = 0;
+  for ( let i = 0; i<array.length; i++) {
+   avgValue += array[i];
+   
 }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+  return avgValue / array.length;
+
+};
